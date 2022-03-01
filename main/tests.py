51323f3081c9c0373293e92ec0abdb1abd950340
Robert-byte-ai <saakyan-robert@rambler.ru>
2022-03-01 17:55:48 +0300

    tests and docs

--- main/tests.py
+++ main/tests.py
@@ -61,12 +61,14 @@ class Tests(APITestCase):
         cls.authorized_client = APIClient()
         cls.authorized_client.force_authenticate(user=cls.user)
 
-    def test_posts(self):
-        response_0 = self.guest_client.get(POST_URL)
+    def test_list_posts(self):
+        response = self.guest_client.get(POST_URL)
         self.assertEqual(
-            response_0.status_code,
+            response.status_code,
             status.HTTP_200_OK
         )
+
+    def test_create_post(self):
         posts_count = Post.objects.count()
         data = {
             'text': 'test',
@@ -75,22 +77,30 @@ class Tests(APITestCase):
         response = self.authorized_client.post(POST_URL, data=data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Post.objects.count(), posts_count + 1)
-        response1 = self.authorized_client.post(self.POST_READ_URL)
-        self.assertEqual(response1.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response1.data.get('post'), self.post.pk)
-        self.assertEqual(response1.data.get('user'), self.user.pk)
-        response2 = self.authorized_client.get(POST_LIST_FILTERED_BY_READ_URL)
-        self.assertEqual(response2.status_code, status.HTTP_200_OK)
-        read_content = dict(*response2.data.get('results'))
+
+    def test_mark_post_read(self):
+        response = self.authorized_client.post(self.POST_READ_URL)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data.get('post'), self.post.pk)
+        self.assertEqual(response.data.get('user'), self.user.pk)
+
+    def test_list_read_posts(self):
+        self.authorized_client.post(self.POST_READ_URL)
+        response = self.authorized_client.get(POST_LIST_FILTERED_BY_READ_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        read_content = dict(*response.data.get('results'))
         self.assertEqual(read_content.get('author'), self.post.author.username)
         self.assertEqual(read_content.get('text'), self.post.text)
         self.assertEqual(read_content.get('title'), self.post.title)
 
-    def test_follow(self):
+    def test_create_follow(self):
         response1 = self.authorized_client.post(FOLLOW_URL, data={'following': self.user2.username})
         self.assertEqual(response1.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response1.data.get('user'), self.user.username)
         self.assertEqual(response1.data.get('following'), self.user2.username)
+
+    def test_list_follow(self):
+        self.authorized_client.post(FOLLOW_URL, data={'following': self.user2.username})
         response2 = self.authorized_client.get(POST_LIST_FILTERED_BY_FOLLOWING_URL)
         follow_content = dict(*response2.data.get('results'))
         self.assertEqual(follow_content.get('author'), self.user2.username)
@@ -114,3 +124,4 @@ class Tests(APITestCase):
                 username=second_user['username']
             ).posts.count()
         )
+
